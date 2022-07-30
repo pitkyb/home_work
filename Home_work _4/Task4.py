13ecc3a6173af6675d7fc1b36db364201ab2af9a
Pitkyb <pitkyb1@gmail.com>
2022-07-30 14:47:47 +0300

    task4

--- Home_work _4/Task4.py
+++ Home_work _4/Task4.py
@@ -3,9 +3,7 @@ if int(year) >= 1900 and int(year) <= 1_000_000:
        print()
 else:
     print('Год не отвечает условиям')
-
-if int(year) % 4 == 0:
+if int(year) % 4 == 0 and int(year) %100 != 0 or int(year) % 400 == 0:
     print(str(year) + " год является высокосным!")
 else:
     print(str(year) + " год является не высокостным!")
-
