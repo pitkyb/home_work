66de898a9cb61617cf7a3a4674740130038e7c79
pitkyb <109979908+pitkyb@users.noreply.github.com>
2022-08-04 20:37:21 +0300

    Delete Task1.py

--- Home_Work_5/Task1.py
+++
@@ -1,9 +0,0 @@
-start = True
-while start == True:
-   num1 = input("Введите число: ")
-   num2 = set(num1)
-   if len(num1) != len(num2):
-      print('Да')
-   elif len(num1) == len(num2):
-      print('НЕТ')
-      start = False
